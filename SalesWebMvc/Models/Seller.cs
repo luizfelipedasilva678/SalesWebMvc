9a2d3cf9c0c5331fe8d0c198753ffbb9832a2dc9
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-04-29 18:48:38 -0300

    Foreign Key not null (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models
@@ -13,6 +14,7 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
